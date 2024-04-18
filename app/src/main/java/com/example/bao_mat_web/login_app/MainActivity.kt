e6f84451fececd4f4885dd846f5ef9f46965d5c7
LazyBlob <21520090@gm.uit.edu.vn>
2024-04-18 21:24:21 +0700

    Just to be sure

--- app/src/main/java/com/example/bao_mat_web/login_app/MainActivity.kt
+++ app/src/main/java/com/example/bao_mat_web/login_app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bao_mat_web.login_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.logoutButton.setOnClickListener {
+            Toast.makeText(this, "Logout Succesful", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
